6d905dc2b605a0ccd8e830536cf14de8d0cae3eb
appu1232 <aditya.vad@gmail.com>
2017-01-11 14:27:15 -0500

    removed logger

--- appuselfbot.py
+++ appuselfbot.py
@@ -5,12 +5,6 @@ import sys
 from discord.ext import commands
 from utils.allmsgs import *
 
-# Discord Logger
-# logger = logging.getLogger('discord')
-# logger.setLevel(logging.DEBUG)
-# handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
-# handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
-# logger.addHandler(handler)
 
 with open('config.json', 'r') as f:
     config = json.load(f)
