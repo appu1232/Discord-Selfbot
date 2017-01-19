e2b435a5e7bab5f42f3e7e7e4cc32aecb7a3034b
appu1232 <aditya.vad@gmail.com>
2017-01-19 13:37:14 -0500

    better mal embed formatting

--- utils/mal.py
+++ utils/mal.py
@@ -56,43 +56,46 @@ class Mal:
         # Formatting embed
         selection = results
         synopsis = BeautifulSoup(selection.synopsis, 'lxml')
-        urlcontent = urllib.request.urlopen("https://myanimelist.net/anime/%s" % selection.id).read()
-        imgurls = re.findall('img .*?src="(.*?)"', str(urlcontent))
 
-        em = discord.Embed(title=selection.title,
-                           description='''\n{link}\n\n**Episodes:** {episodes}\n**Avg Score:** {score}/10\n**Synopsis:**{synopsis}\n'''.format(
-                               link="https://myanimelist.net/anime/%s" % selection.id, title=selection.title,
-                               episodes=selection.episodes, score=selection.score, synopsis="\n" + synopsis.get_text()[
-                                                                                                   :400] + '...[more](https://myanimelist.net/anime/%s)' % selection.id),
+        em = discord.Embed(description='{}'.format('https://myanimelist.net/anime/%s' % selection.id),
                            colour=0x0066CC)
+
         try:
-            em.add_field(name='Type', value=selection.anime_type, inline=True)
-            em.add_field(name='English', value=selection.english, inline=True)
-            em.add_field(name='Status:', value=selection.status, inline=True)
-            em.add_field(name='Airing Time:', value=selection.dates[0] + "  -  " + selection.dates[1], inline=True)
+            english = selection.english
+            if english:
+                em.add_field(name='English Title', value=english, inline=False)
+        except:
+            pass
+        em.add_field(name='Type', value=selection.anime_type, inline=True)
+        if selection.episodes == '0':
+            episodes = 'Unknown'
+        else:
+            episodes = selection.episodes
+        em.add_field(name='Episodes', value=episodes, inline=True)
+        em.add_field(name='Score', value=selection.score + '/10', inline=True)
+        em.add_field(name='Status', value=selection.status, inline=True)
+        try:
+            synop = synopsis.get_text()[:400].split('.')
+            text = ''
+            for i in range(0, len(synop)-1):
+                text += synop[i] + '.'
+        except:
+            text = synopsis.get_text()
+        em.add_field(name='Synopsis',
+                     value=text + ' [Read more »](https://myanimelist.net/anime/%s)' % selection.id)
+        try:
+            if selection.dates[1] == '0000-00-00':
+                date = selection.dates[0] + "  -  "
+            else:
+                date = selection.dates[0] + "  -  " + selection.dates[1]
+            if date:
+                em.add_field(name='Airing Time:', value=date, inline=True)
         except:
             pass
         em.set_thumbnail(url=selection.image_url)
-        em.set_author(name='MyAnimeList',
+        em.set_author(name=selection.title,
                       icon_url='https://myanimelist.cdn-dena.com/img/sp/icon/apple-touch-icon-256.png')
-        try:
-            await self.bot.send_message(ctx.message.channel, embed=em)
-
-        # Yea, I know this is weird but it had to be done. Blame the mal api.
-        except:
-            em = discord.Embed(title=selection.title,
-                               description='''\n{link}\n\n**Episodes:** {episodes}\n**Avg Score:** {score}/10\n**Synopsis:**{synopsis}\n'''.format(
-                                   link="https://myanimelist.net/anime/%s" % selection.id, title=selection.title,
-                                   episodes=selection.episodes, score=selection.score,
-                                   synopsis="\n" + synopsis.get_text()[
-                                                   :400] + '...[more](https://myanimelist.net/anime/%s)' % selection.id),
-                               colour=0x0066CC)
-            em.add_field(name='Type', value=selection.anime_type, inline=True)
-            em.add_field(name='Status:', value=selection.status, inline=True)
-            em.set_thumbnail(url=selection.image_url)
-            em.set_author(name='MyAnimeList', icon_url='https://myanimelist.cdn-dena.com/img/sp/icon/apple-touch-icon-256.png')
-            await self.bot.send_message(ctx.message.channel, embed=em)
-
+        await self.bot.send_message(ctx.message.channel, embed=em)
 
     # Manga search for Mal
     @mal.command(pass_context=True)
@@ -129,39 +132,49 @@ class Mal:
         # Formatting
         selection = results
         synopsis = BeautifulSoup(selection.synopsis, 'lxml')
-        em = discord.Embed(title=selection.title,
-                           description='''\n{link}\n\n**Chapters:** {chapters}\n**Avg Score:** {score}/10\n**Synopsis:**{synopsis}\n'''.format(
-                               link="https://myanimelist.net/manga/%s" % selection.id, title=selection.title,
-                               chapters=selection.chapters, score=selection.score, synopsis="\n" + synopsis.get_text()[
-                                                                                                   :400] + '...[more](https://myanimelist.net/manga/%s)' % selection.id),
+        em = discord.Embed(description='{}'.format('https://myanimelist.net/manga/%s' % selection.id),
                            colour=0x0066CC)
+
+        em.add_field(name='Type', value=selection.manga_type, inline=True)
+        if selection.chapters == '0':
+            chapters = 'Unknown'
+        else:
+            chapters = selection.chapters
+        em.add_field(name='Chapters', value=chapters, inline=True)
+        print(selection.chapters)
+        print(type(selection.chapters))
+        em.add_field(name='Score', value=selection.score + '/10', inline=True)
         try:
-            em.add_field(name='Type', value=selection.manga_type, inline=True)
-            em.add_field(name='English', value=selection.english, inline=True)
-            em.add_field(name='Status:', value=selection.status, inline=True)
-            em.add_field(name='Publishing Time:', value=selection.dates[0] + "  -  " + selection.dates[1], inline=True)
+            english = selection.english
+            if english:
+                em.add_field(name='English Title', value=english, inline=False)
         except:
             pass
-        em.set_thumbnail(url=selection.image_url)
-        em.set_author(name='MyAnimeList',
-                      icon_url='https://myanimelist.cdn-dena.com/img/sp/icon/apple-touch-icon-256.png')
-        try:
-            await self.bot.send_message(ctx.message.channel, embed=em)
 
-        # Again, really weird but it had to be done. Blame the mal api.
+        em.add_field(name='Status', value=selection.status, inline=True)
+        try:
+            synop = synopsis.get_text()[:400].split('.')
+            text = ''
+            for i in range(0, len(synop) - 1):
+                text += synop[i] + '.'
         except:
-            em = discord.Embed(title=selection.title,
-                               description='''\n{link}\n\n**Chapters:** {chapters}\n**Avg Score:** {score}/10\n**Synopsis:**{synopsis}\n'''.format(
-                                   link="https://myanimelist.net/manga/%s" % selection.id, title=selection.title,
-                                   chapters=selection.chapters, score=selection.score,
-                                   synopsis="\n" + synopsis.get_text()[
-                                                   :400] + '...[more](https://myanimelist.net/manga/%s)' % selection.id),
-                               colour=0x0066CC)
-            em.add_field(name='Type', value=selection.manga_type, inline=True)
-            em.add_field(name='Status:', value=selection.status, inline=True)
-            em.set_thumbnail(url=selection.image_url)
-            em.set_author(name='MyAnimeList', icon_url='https://myanimelist.cdn-dena.com/img/sp/icon/apple-touch-icon-256.png')
-            await self.bot.send_message(ctx.message.channel, embed=em)
+            text = synopsis.get_text()
+        em.add_field(name='Synopsis',
+                     value=text + ' [Read more »](https://myanimelist.net/anime/%s)' % selection.id)
+        try:
+            if selection.dates[1] == '0000-00-00':
+                date = selection.dates[0] + "  -  "
+            else:
+                date = selection.dates[0] + "  -  " + selection.dates[1]
+            if date:
+                em.add_field(name='Publishing Time:', value=date, inline=True)
+        except:
+            pass
+
+        em.set_thumbnail(url=selection.image_url)
+        em.set_author(name=selection.title,
+                      icon_url='https://myanimelist.cdn-dena.com/img/sp/icon/apple-touch-icon-256.png')
+        await self.bot.send_message(ctx.message.channel, embed=em)
 
 
 def setup(bot):
