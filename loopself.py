7f229e18c497972f8ef2655d40a000c28bcbb455
appu1232 <aditya.vad@gmail.com>
2017-01-27 14:36:45 -0500

    fixed for linux

--- loopself.py
+++ loopself.py
@@ -2,6 +2,8 @@ import subprocess, traceback
 
 while True:
     try:
+        p = subprocess.call(['python3', 'appuselfbot.py'])
+    except (SyntaxError, FileNotFoundError):
         p = subprocess.call(['python', 'appuselfbot.py'])
     except:
         traceback.print_exc()
