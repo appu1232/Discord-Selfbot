d676a65c8c55506bbc086e1152d1c5c848480496
appu1232 <aditya.vad@gmail.com>
2017-03-05 18:58:27 -0500

    fixed for req install

--- run.bat
+++ run.bat
@@ -1,5 +1,5 @@
 @echo off
-echo Starting bot...
+echo Starting bot (this may take a minute or two)...
 FOR /f %%p in ('where python') do SET PYTHONPATH=%%p
 python -m pip install -r requirements.txt
 python loopself.py
