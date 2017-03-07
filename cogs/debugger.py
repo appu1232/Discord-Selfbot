e4c4c1ff42e1829b0788ab4514df0224ca4b4f01
appu1232 <aditya.vad@gmail.com>
2017-03-07 17:53:15 -0500

    v1.5.3 fixed exec and eval for debugger

--- cogs/debugger.py
+++ cogs/debugger.py
@@ -41,11 +41,11 @@ class Debugger:
     def __init__(self, bot):
         self.bot = bot
 
-    # Standalone can be used for quick evals/execs. Group with make/load/save to use full-fledged scripts.
+    # Standalone can be used for quick evals/execs.
     @commands.group(pass_context=True)
     async def py(self, ctx):
 
-        # Got the idea from RoboDanny bot by Rapptz. RoboDanny uses eval() but I use exec() to cover a wider scope of possible inputs. make/load/save subcommands allow for an even wider scope.
+        # Got the idea from RoboDanny bot by Rapptz. RoboDanny uses eval() but I use exec() to cover a wider scope of possible inputs.
         if ctx.invoked_subcommand is None:
             code = ctx.message.content[3:].strip('` ')
             python = '```py\n{}\n```'
@@ -60,23 +60,47 @@ class Debugger:
             }
             env.update(globals())
             try:
-                with stdoutIO() as s:
-                    result = exec(code, env)
-                    if inspect.isawaitable(result):
-                        result = await result
-                result = s.getvalue()
-                if result == '':
-                    code = 'print(' + code + ')'
+                result = eval(code, env)
+                if inspect.isawaitable(result):
+                    result = await result
+            except SyntaxError:
+                try:
                     with stdoutIO() as s:
                         result = exec(code, env)
                         if inspect.isawaitable(result):
                             result = await result
                     result = s.getvalue()
+                except Exception as g:
+                    return await self.bot.send_message(ctx.message.channel, appuselfbot.isBot + python.format(type(g).__name__ + ': ' + str(g)))
+
             except Exception as e:
-                await self.bot.send_message(ctx.message.channel, appuselfbot.isBot + python.format(type(e).__name__ + ': ' + str(e)))
-                return
+                return await self.bot.send_message(ctx.message.channel, appuselfbot.isBot + python.format(type(e).__name__ + ': ' + str(e)))
 
             await self.bot.send_message(ctx.message.channel, appuselfbot.isBot + python.format(result))
+
+    # Load an extension
+    @commands.command(pass_context=True)
+    async def load(self, ctx, *, msg):
+        try:
+            self.bot.load_extension(msg)
+        except Exception as e:
+            await self.bot.send_message(ctx.message.channel, appuselfbot.isBot + 'Failed to load module: `{}`'.format(msg))
+            await self.bot.send_message(ctx.message.channel, appuselfbot.isBot + '{}: {}'.format(type(e).__name__, e))
+        else:
+            await self.bot.send_message(ctx.message.channel, appuselfbot.isBot + 'Loaded module: `{}`'.format(msg))
+        await self.bot.delete_message(ctx.message)
+
+    # Unload an extension
+    @commands.command(pass_context=True)
+    async def unload(self, ctx, *, msg):
+        try:
+            self.bot.unload_extension(msg)
+        except Exception as e:
+            await self.bot.send_message(ctx.message.channel, appuselfbot.isBot + 'Failed to unload module: `{}`'.format(msg))
+            await self.bot.send_message(ctx.message.channel, appuselfbot.isBot + '{}: {}'.format(type(e).__name__, e))
+        else:
+            await self.bot.send_message(ctx.message.channel, appuselfbot.isBot + 'Unloaded module: `{}`'.format(msg))
+        await self.bot.delete_message(ctx.message)
 
 
 def setup(bot):
