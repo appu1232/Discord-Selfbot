dd2f747442a50f2c71936774ff89d91bbebb2988
appu1232 <aditya.vad@gmail.com>
2017-03-24 17:21:54 -0400

    fixed for >quit command

--- loopself.py
+++ loopself.py
@@ -1,6 +1,11 @@
-import subprocess, traceback
+import subprocess
+import traceback
+import os
 
 while True:
+    if os.path.isfile('quit.txt'):
+        os.remove('quit.txt')
+        break
     try:
         p = subprocess.call(['python3', 'appuselfbot.py'])
     except (SyntaxError, FileNotFoundError):
