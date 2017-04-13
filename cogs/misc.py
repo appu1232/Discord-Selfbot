8341c4f20e1e06a83eea6eef2b9dc4c1406d4e14
appu1232 <aditya.vad@gmail.com>
2017-04-13 15:08:21 -0400

    :ambulance: fix for urls not being detected in imagedump cmd, oops

--- cogs/misc.py
+++ cogs/misc.py
@@ -405,7 +405,7 @@ class Misc:
                             urls = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', message.content)
                         except:
                             pass
-                        if urls is []:
+                        if urls is not []:
                             for url in urls:
                                 if url.endswith(('.jpg', '.jpeg', '.png', '.gif', '.gifv', '.webm')):
                                     messages.append(url)
