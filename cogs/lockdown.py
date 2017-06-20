788b21c2d4a079cccd43160a9ba426118a6fca2f
LyricLy <hanson.twentytwo@gmail.com>
2017-06-20 21:09:02 +1200

    add commands to edit moderation.json

--- cogs/lockdown.py
+++ cogs/lockdown.py
@@ -8,7 +8,7 @@ class Lockdown:
     """
     Channel lockdown commands.
 
-    To give specific roles permissions to bypass lockdown, open `moderation.json` file in the settings folder 
+    To give specific roles permissions to bypass lockdown, open `moderation.json` file in the settings folder
     make an entry of the server name as the key
     make an entry of the list of role names as the value
     """
@@ -69,6 +69,68 @@ class Lockdown:
             await self.bot.say("🔓 Channel unlocked.")
        except discord.errors.Forbidden:
             await self.bot.say("Missing Permissions.")
+
+    @commands.group(pass_context=True)
+    async def mod(self, ctx):
+        """Manage list of moderator roles for the >lockdown command. >help mod for more info.
+        >mod - List your moderator roles that you have set.
+        >mod add <server> <role> - Add a role to the list of moderators on a server.
+        >mod remove <server> <role> - Remove a role from the list of moderators on a server.
+        If a server or role name has spaces in it, you must enclose *both* of them in quotes, no matter which one is the one with spaces in it.
+        """
+        if ctx.invoked_subcommand is None:
+            await self.bot.delete_message(ctx.message)
+            mods = load_moderation()
+            embed = discord.Embed(title="Moderator Roles", description="")
+            for server in mods:
+                embed.description += server + ":\n"
+                for mod in mods["server"]:
+                    embed.description += "    {}\n".format(mod)
+            await self.bot.send_message(ctx.message.channel, "", embed=embed)
+
+    @mod.command(pass_context=True)
+    async def add(self, ctx, server, role):
+        """Add a role to the list of moderators on a server.
+        If a server or role name has spaces in it, you must enclose *both* of them in quotes, no matter which one is the one with spaces in it.
+        """
+        mods = load_moderation()
+        valid_server = False
+        valid_role = False
+        for e in self.bot.servers:
+            if e.name == server:
+                valid_server = True
+            for f in e.roles:
+                if f.name == role:
+                    valid_role = True
+        if valid_server:
+            if valid_role:
+                try:
+                    mods[server]
+                except KeyError:
+                    mods[server] = [role]
+                else:
+                    mods[server].append(role)
+                with open("settings/moderation.json", "w+") as f:
+                    json.dump(mods, f)
+                await self.bot.send_message(ctx.message.channel, bot_prefix + "Successfully added {} to the list of mod roles on {}!".format(role, server))
+            else:
+                await self.bot.send_message(ctx.message.channel, bot_prefix + "{} isn't a role on {}!".format(role, server))
+        else:
+            await self.bot.send_message(ctx.message.channel, bot_prefix + "{} isn't a server!".format(server))
+
+    @mod.command(pass_context=True)
+    async def remove(self, ctx, server, role):
+        """Remove a role from the list of moderators on a server.
+        If a server or role name has spaces in it, you must enclose *both* of them in quotes, no matter which one is the one with spaces in it.
+        """
+        mods = load_moderation()
+        try:
+            mods[server].remove(role)
+            with open("settings/moderation.json", "w+") as f:
+                json.dump(mods, f)
+            await self.bot.send_message(ctx.message.channel, bot_prefix + "Successfully removed {} from the list of mod roles on {}!".format(role, server))
+        except (ValueError, KeyError):
+            await self.bot.send_message(ctx.message.channel, bot_prefix + "You can't remove something that doesn't exist!")        
 
 def setup(bot):
     bot.add_cog(Lockdown(bot))
