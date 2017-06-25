2e9fcd9bd70fcb93d35b3c0fbda253754a54e446
appu1232 <aditya.vad@gmail.com>
2017-06-25 10:14:11 -0400

    :ambulance: fixed fatal missing ) in l2g

--- cogs/fun.py
+++ cogs/fun.py
@@ -192,7 +192,7 @@ class Fun:
     async def l2g(self, ctx, *, msg: str):
         """Creates a lmgtfy link. Ex: >l2g how do i become cool."""
         lmgtfy = 'http://lmgtfy.com/?q='
-        await self.bot.send_message(ctx.message.channel, bot_prefix + lmgtfy + urllib.parse.quote_plus(msg.lower().strip())
+        await self.bot.send_message(ctx.message.channel, bot_prefix + lmgtfy + urllib.parse.quote_plus(msg.lower().strip()))
         await self.bot.delete_message(ctx.message)
 
     @commands.command(pass_context=True)
