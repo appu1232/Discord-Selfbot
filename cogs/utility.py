d13d00fd4a4f0b4f956ed14a329f19011829ea51
LyricLy <hanson.twentytwo@gmail.com>
2017-06-29 20:05:16 +1200

    put message dumps in a seperate folder

--- cogs/utility.py
+++ cogs/utility.py
@@ -667,7 +667,7 @@ class Utility:
         """Dump messages."""
         await self.bot.delete_message(ctx.message)
         await self.bot.send_message(ctx.message.channel, self.bot.bot_prefix + "Downloading messages...")
-        with open(filename.rsplit('.', 1)[0] + ".txt", "w+") as f:
+        with open("message_dump/" + filename.rsplit('.', 1)[0] + ".txt", "w+") as f:
             if reverse == "yes":
                 async for message in self.bot.logs_from(ctx.message.channel, int(limit)):
                     f.write(message.content + "\n")
