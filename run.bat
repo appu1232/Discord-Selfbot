1daf151cbc22b2b074b0562638aeb3b6c1ed45b2
appu1232 <aditya.vad@gmail.com>
2017-07-17 00:28:16 -0400

    better req installation help

--- run.bat
+++ run.bat
@@ -68,15 +68,26 @@ goto run
 	goto :EOF
 :run
 	if exist tmp.txt del tmp.txt
-	type cogs\utils\credit.txt
-	echo[
-	echo[
 	FOR /f %%p in ('where python') do SET PYTHONPATH=%%p
 	echo Checking/Installing requirements (takes some time on first install)...
 	chcp 65001 >nul
 	set PYTHONIOENCODING=utf-8
 	python -m pip install --upgrade pip >nul
-	python -m pip install -r requirements.txt >nul
+	python -m pip install -r requirements.txt
+	if errorlevel 1 (
+	    echo Requirements installation failed. Perhaps some dependency is missing or access was denied? Possible solutions:
+	    echo - Run as administrator
+	    echo - Google the error
+	    echo - Visit the Discord server for help
+	    echo Press any key to exit.
+	    set /p input=
+	    exit
+	)
+	ping 127.0.0.1 -n 2 > nul
+	cls
+	type cogs\utils\credit.txt
+	echo[
+	echo[
 	echo Requirements satisfied.
 	echo Starting the bot (this may take a minute or two)...
 	python loopself.py
