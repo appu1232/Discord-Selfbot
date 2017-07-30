ac51a14627d52cdba079ac933898f6952fbef2d5
LyricLy <hanson.twentytwo@gmail.com>
2017-07-30 15:25:40 +1200

    Rewrite server cog

--- cogs/server.py
+++ cogs/server.py
@@ -18,7 +18,7 @@ class Server:
         if msg:
             try:
                 float(msg)
-                server = self.bot.get_server(msg)
+                server = self.bot.get_server(int(msg))
                 if not server:
                     return self.bot.bot_prefix + 'Server not found.', False
             except:
@@ -32,28 +32,28 @@ class Server:
         return server, True
 
     # Stats about server
-    @commands.group(pass_context=True)
-    async def server(self, ctx):
+    @commands.group(pass_context=True, invoke_without_command=True)
+    async def server(self, ctx, *, msg=""):
         """Various info about the server. >help server for more info."""
         if ctx.invoked_subcommand is None:
             pre = cmd_prefix_len()
-            if ctx.message.content[6 + pre:].strip():
+            if msg:
                 server = None
                 try:
-                    float(ctx.message.content[6 + pre:].strip())
-                    server = self.bot.get_server(ctx.message.content[6 + pre:].strip())
+                    float(msg)
+                    server = self.bot.get_guild(int(msg))
                     if not server:
-                        return await self.bot.send_message(ctx.message.channel,
-                                                           self.bot.bot_prefix + 'Server not found.')
+                        return await ctx.send(
+                                              self.bot.bot_prefix + 'Server not found.')
                 except:
-                    for i in self.bot.servers:
-                        if i.name.lower() == ctx.message.content[6 + pre:].lower().strip():
+                    for i in self.bot.guilds:
+                        if i.name.lower() == msg.lower():
                             server = i
                             break
                     if not server:
-                        return await self.bot.send_message(ctx.message.channel, self.bot.bot_prefix + 'Could not find server. Note: You must be a member of the server you are trying to search.')
+                        return await ctx.send(self.bot.bot_prefix + 'Could not find server. Note: You must be a member of the server you are trying to search.')
             else:
-                server = ctx.message.server
+                server = ctx.message.guild
 
             online = 0
             for i in server.members:
@@ -65,10 +65,7 @@ class Server:
             all_users.sort()
             all = '\n'.join(all_users)
 
-            channel_count = 0
-            for channel in server.channels:
-                if channel.type == discord.ChannelType.text:
-                    channel_count += 1
+            channel_count = len([x for x in server.channels if type(x) == discord.channel.TextChannel])
 
             role_count = len(server.roles)
             emoji_count = len(server.emojis)
@@ -92,115 +89,70 @@ class Server:
                 em.set_thumbnail(url=server.icon_url)
                 em.set_author(name='Server Info', icon_url='https://i.imgur.com/RHagTDg.png')
                 em.set_footer(text='Server ID: %s' % server.id)
-                await self.bot.send_message(ctx.message.channel, embed=em)
+                await ctx.send(embed=em)
             else:
                 msg = '**Server Info:** ```Name: %s\nOwner: %s\nMembers: %s\nCurrently Online: %s\nRegion: %s\nVerification Level: %s\nHighest Role: %s\nDefault Channel: %s\nCreated At: %s\nServer avatar: : %s```' % (
                 server.name, server.owner, server.member_count, online, server.region, str(server.verification_level), server.role_hierarchy[0], server.default_channel, server.created_at.__format__('%A, %d. %B %Y @ %H:%M:%S'), server.icon_url)
-                await self.bot.send_message(ctx.message.channel, self.bot.bot_prefix + msg)
-            await self.bot.delete_message(ctx.message)
+                await ctx.send(self.bot.bot_prefix + msg)
+            await ctx.message.delete()
 
-    @server.group(pass_context=True)
+    @server.command(pass_context=True)
     async def emojis(self, ctx, msg: str = None):
         """List all emojis in this server. Ex: >server emojis"""
         if msg:
             server, found = self.find_server(msg)
+            if not found:
+                return await ctx.send(server)
         else:
-            server = ctx.message.server
-        emojis = ''
-        for i in server.emojis:
-            emojis += str(i)
-        await self.bot.send_message(ctx.message.channel, emojis)
-        await self.bot.delete_message(ctx.message)
+            server = ctx.message.guild
+        emojis = [str(x) for x in server.emojis]
+        await ctx.send("".join(emojis))
+        await ctx.message.delete()
 
-    @server.group(pass_context=True)
+    @server.command(pass_context=True)
     async def avi(self, ctx, msg: str = None):
         """Get server avatar image link."""
         if msg:
             server, found = self.find_server(msg)
+            if not found:
+                return await ctx.send(server)
         else:
-            server = ctx.message.server
+            server = ctx.message.guild
         if embed_perms(ctx.message):
             em = discord.Embed()
             em.set_image(url=server.icon_url)
-            await self.bot.send_message(ctx.message.channel, embed=em)
+            await ctx.send(embed=em)
         else:
-            await self.bot.send_message(ctx.message.channel, self.bot.bot_prefix + server.icon_url)
-        await self.bot.delete_message(ctx.message)
+            await ctx.send(self.bot.bot_prefix + server.icon_url)
+        await ctx.message.delete()
 
-    @server.group(pass_context=True)
+    @server.command(pass_context=True)
     async def role(self, ctx, *, msg):
         """Get more info about a specific role. Ex: >server role Admins"""
-        for role in ctx.message.server.roles:
+        for role in ctx.message.guild.roles:
             if msg == role.name or msg == role.id:
-                role_count = 0
-                all_users = []
-                for user in ctx.message.server.members:
-                    if role in user.roles:
-                        all_users.append('{}#{}'.format(user.name, user.discriminator))
-                        role_count += 1
+                all_users = [str(x) for x in role.members]
                 all_users.sort()
                 all_users = ', '.join(all_users)
                 em = discord.Embed(title='Role Info', color=role.color)
                 em.add_field(name='Name', value=role.name)
                 em.add_field(name='ID', value=role.id, inline=False)
-                em.add_field(name='Users in this role', value=role_count)
+                em.add_field(name='Users in this role', value=len(role.members))
                 em.add_field(name='Role color hex value', value=str(role.color))
-                em.add_field(name='Role color rgb value', value=role.color.to_tuple())
+                em.add_field(name='Role color RGB value', value=role.color.to_rgb())
                 em.add_field(name='Mentionable', value=role.mentionable)
-                if role_count > 10:
+                if len(role.members) > 10:
                     all_users = all_users.replace(', ', '\n')
-                    url = PythonGists.Gist(description='Users in role: {} for server: {}'.format(role.name, ctx.message.server.name), content=str(all_users), name='role.txt')
-                    em.add_field(name='All users', value='{} users. [List of users posted to Gist.]({})'.format(role_count, url), inline=False)
+                    url = PythonGists.Gist(description='Users in role: {} for server: {}'.format(role.name, ctx.message.guild.name), content=str(all_users), name='role.txt')
+                    em.add_field(name='All users', value='{} users. [List of users posted to Gist.]({})'.format(len(role.members), url), inline=False)
                 else:
-                    em.add_field(name='All Users', value=all_users, inline=False)
+                    em.add_field(name='All users', value=all_users, inline=False)
                 em.add_field(name='Created at', value=role.created_at.__format__('%x at %X'))
                 em.set_thumbnail(url='http://www.colorhexa.com/%s.png' % str(role.color).strip("#"))
-                return await self.bot.send_message(ctx.message.channel, content=None, embed=em)
-        await self.bot.send_message(ctx.message.channel, self.bot.bot_prefix + 'Could not find role ``%s``' % msg)
-        await self.bot.delete_message(ctx.message)
-
-    @server.group(pass_context=True)
-    async def members(self, ctx, msg: str = None):
-        """List of members in the server."""
-        if msg:
-            server, found = self.find_server(msg)
-        else:
-            server = ctx.message.server
-        msg = prettytable.PrettyTable(['User', 'Nickname', 'Join Date', 'Account Created', 'Color', 'Top Role', 'Is bot', 'Avatar url', 'All Roles'])
-        for i in server.members:
-            roles = ''
-            for j in i.roles:
-                if j.name != '@everyone':
-                    roles += j.name + ', '
-            if i.avatar_url[60:].startswith('a_'):
-                avi = 'https://images.discordapp.net/avatars/' + i.avatar_url[33:][:18] + i.avatar_url[59:-3] + 'gif'
-            else:
-                avi = i.avatar_url
-            try:
-                join = i.joined_at.__format__('%x at %X')
-            except:
-                join = 'N/A'
-            try:
-                create = i.created_at.__format__('%x at %X')
-            except:
-                create = 'N/A'
-            msg.add_row([str(i.name), i.nick, join, create, i.color, i.top_role, str(i.bot), avi, roles[:-2]])
-        name = server.name
-        keep_characters = (' ', '.', '_')
-        name = ''.join(c for c in name if c.isalnum() or c in keep_characters).rstrip()
-        name = name.replace(' ', '_')
-        save_file = '%s_members.txt' % name
-        try:
-            msg = msg.get_string(sortby='User')
-        except:
-            pass
-        with open(save_file, 'w') as file:
-            file.write(str(str(msg).encode('ascii', 'ignore')))
-        with open(save_file, 'rb') as file:
-            await self.bot.send_file(ctx.message.channel, file)
-        os.remove(save_file)
-        await self.bot.delete_message(ctx.message)
-
+                await ctx.message.delete()
+                return await ctx.send(content=None, embed=em)
+        await ctx.message.delete()
+        await ctx.send(self.bot.bot_prefix + 'Could not find role ``%s``' % msg)
 
 def setup(bot):
     bot.add_cog(Server(bot))
