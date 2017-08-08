41f8282de2e1e80a12cc17a3872c87136912d8bd
LyricLy <hanson.twentytwo@gmail.com>
2017-08-08 12:54:52 +1200

    Menu class

--- cogs/replacements.py
+++ cogs/replacements.py
@@ -2,6 +2,7 @@ import discord
 import json
 from discord.ext import commands
 from cogs.utils.dataIO import dataIO
+from cogs.utils.menu import Menu
 
 '''Manage replacements within messages.'''
 
@@ -10,57 +11,38 @@ class Replacements:
 
     def __init__(self, bot):
         self.bot = bot
-        try:
-            with open("settings/replacements.json", encoding='utf-8') as rp:
-                self.replacement_dict = json.load(rp)
-        except FileNotFoundError:
-            self.replacement_dict = {}
+        self.replacement_dict = dataIO.load_json("settings/replacements.json")
 
     @commands.command(pass_context=True)
     async def replacements(self, ctx):
-        """Set text you want to automatically replace with something else."""
         await ctx.message.delete()
-        menu_msg = await ctx.send("```\nWhat would you like to do? Pick a number.\n\n1. Create a new replacement\n2. Remove an existing replacement\n3. List all current replacements```")
-        reply = await self.bot.wait_for("message", check=lambda m: m.channel == ctx.message.channel and m.author == self.bot.user and m.content.isdigit())
-        await reply.delete()
-        if int(reply.content) == 1:
-            await menu_msg.edit(content="```\nEnter a replacement trigger.\n```")
-            reply = await self.bot.wait_for("message", check=lambda m: m.channel == ctx.message.channel and m.author == self.bot.user)
-            await reply.delete()
-            trigger = reply.content
-            await menu_msg.edit(content="```\nEnter a string to replace the trigger with.\n```")
-            reply = await self.bot.wait_for("message", check=lambda m: m.channel == ctx.message.channel and m.author == self.bot.user)
-            await reply.delete()
-            replacement = reply.content
-            self.replacement_dict[trigger] = replacement
+        menu = Menu("What would you like to do?")
+        
+        
+        # handle new replacements
+        def new_replacement(trigger, val):
+            self.replacement_dict[trigger.content] = val.content
             with open("settings/replacements.json", "w+") as f:
                 json.dump(self.replacement_dict, f, sort_keys=True, indent=4)
-            await menu_msg.edit(content="```\nSuccessfully added a {}/{} replacement!\n```".format(trigger, replacement))
-        elif int(reply.content) == 2:
-            if self.replacement_dict:
-                wizard_msg = "```\nPick a replacement to remove.\n\n"
-                indexes = {}
-                for idx, replacement in enumerate(self.replacement_dict):
-                    wizard_msg += "{}. {}/{}\n".format(idx+1, replacement, self.replacement_dict[replacement])
-                    indexes[idx] = replacement
-                wizard_msg += "```"
-                await menu_msg.edit(content=wizard_msg)
-                reply = await self.bot.wait_for("message", check=lambda m: m.channel == ctx.message.channel and m.author == self.bot.user and m.content.isdigit())
-                await reply.delete()
-                try:
-                    removed_replacement = self.replacement_dict.pop(indexes[int(reply.content)-1])
-                    with open("settings/replacements.json", "w+") as f:
-                        json.dump(self.replacement_dict, f, sort_keys=True, indent=4)
-                    await menu_msg.edit(content="```\nSuccessfully removed the {}/{} replacement!\n```".format(indexes[int(reply.content)-1], removed_replacement))
-                except (IndexError, KeyError):
-                    await menu_msg.edit(content="```\nInvalid number!\n```")
-            else:
-                await menu_msg.edit(content="```You have no replacements to remove!```")
-        elif int(reply.content) == 3:
-            reply_msg = "```All replacements:\n"
-            for replacement in self.replacement_dict:
-                reply_msg += replacement + ": " + self.replacement_dict[replacement] + "\n"
-            await menu_msg.edit(content=reply_msg + "```")
+        
+        end = menu.Submenu("end", "Successfully added a new replacement!")
+        
+        menu.add_child(menu.InputSubmenu("Add a new replacement", ["Enter a replacement trigger.", "Enter a string to replace the trigger with."], new_replacement, end))
+
+        # handle removing replacements
+        def remove_replacement(idx, val):
+            self.replacement_dict.pop(val)
+            with open("settings/replacements.json", "w+") as f:
+                json.dump(self.replacement_dict, f, sort_keys=True, indent=4)
+            
+        end = menu.Submenu("end", "Successfully removed a replacement!")
+        menu.add_child(menu.ChoiceSubmenu("Remove a replacement", "Pick a replacement to remove.", self.replacement_dict, remove_replacement, end))
+        
+        # handle listing replacements
+        menu.add_child(menu.Submenu("List all your replacements", "\n".join([replacement + ": " + self.replacement_dict[replacement] for replacement in self.replacement_dict])))
+        
+        # go
+        await menu.start(ctx)
 
     async def on_message(self, message):
         if message.author == self.bot.user:
@@ -69,7 +51,6 @@ class Replacements:
                 replaced_message = replaced_message.replace(replacement, self.replacement_dict[replacement])
             if message.content != replaced_message:
                 await message.edit(content=replaced_message)
-
 
 def setup(bot):
     bot.add_cog(Replacements(bot))
