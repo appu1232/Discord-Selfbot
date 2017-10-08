76370d8debc00b1ef9f86b70e9aba5b5f7d6735d
Alex Taber <astronautlevel2@users.noreply.github.com>
2017-10-08 10:38:29 -0400

    Sketchy work around to fix translate command (#219)

--- cogs/translate.py
+++ cogs/translate.py
@@ -27,8 +27,8 @@ class Translate:
             embed.add_field(name="Original", value=msg, inline=False)
             embed.add_field(name="ROT13", value=codecs.encode(msg, "rot_13"), inline=False)
             return await ctx.send("", embed=embed)
-        async with self.session.get("http://lyricly.tk/langs.json") as resp:
-            lang_codes = await resp.json()
+        async with self.session.get("https://gist.githubusercontent.com/astronautlevel2/93a19379bd52b351dbc6eef269efa0bc/raw/18d55123bc85e2ef8f54e09007489ceff9b3ba51/langs.json") as resp:
+            lang_codes = await resp.json(content_type='text/plain')
         real_language = False
         to_language = to_language.lower()
         for entry in lang_codes:
@@ -38,7 +38,8 @@ class Translate:
                 real_language = True
         if real_language:
             async with self.session.get("https://translate.google.com/m",
-                                        params={"hl": to_language, "sl": "auto", "q": msg}) as resp:
+                                        params={"hl": to_language, "sl": "auto", "q": msg},
+                                        headers={"User-Agent": "AppuSelfBot"}) as resp:
                 translate = await resp.text()
             result = str(translate).split('class="t0">')[1].split("</div>")[0]
             result = BeautifulSoup(result, "lxml").text
