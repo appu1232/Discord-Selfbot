e22d220288b736fb8d0775b1a74d957e7c8e4935
chenzw <chenzw25@gmail.com>
2017-12-16 02:32:34 +0800

    help module: check against ABC instead in pm_check()

--- cogs/help.py
+++ cogs/help.py
@@ -61,7 +61,7 @@ class Help(formatter.HelpFormatter):
     # Shortcuts that allow cog to run on 0.16.8 and 1.0.0a
 
     def pm_check(self, ctx):
-        return isinstance(ctx.channel, discord.DMChannel) or isinstance(ctx.channel, discord.GroupChannel)
+        return isinstance(ctx.channel, discord.abc.PrivateChannel)
 
     @property
     def me(self):
